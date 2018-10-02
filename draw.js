da35495e39993310e7d97bf83da0038766c8b0b5
ttrexler <42849448+ttrexler@users.noreply.github.com>
2018-10-02 14:26:30 -0500

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,12 +1,15 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth,windowHeight)//(800, 600);
     background(255);
 }
 
 function draw() {
-    stroke(1);
-    if (mouseIsPressed === true) {
-        line(mouseX, mouseY, pmouseX, pmouseY);
-        console.log(touches[mouseY]);
-    }
+    if(mouseIsPressed)cursorcontrol(mouseX, mouseY, pmouseX, pmouseY);
 }
+
+function cursorcontrol(x, y, px, py) {
+    var speed =  abs(y-py) + abs(x-px);//change in cursor location
+    stroke('black');
+    ellipse(x, y, 50-speed, 50-speed);//gives location,dimension to draw ellipse
+    fill(0);
+}//issue with ellipse sizes if you move quickly between 2 near locations
